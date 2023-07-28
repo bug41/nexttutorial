5b066a74f119c0c9854997f6f458e540477b974f
bug41 <bugkinguswit@gmail.com>
2023-07-28 23:35:23 +0900

    중간저장

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import styles from './page.module.css'
+import Image from 'next/image'
+import Button from '@/components/Button/Button'
 
 const About = () => {
   return (
-    <div className={styles.container}>About</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/17636977/pexels-photo-17636977.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          fill={true}
+          alt=""
+          className={styles.img}
+        />
+        <div className={styles.imgText}>
+          <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+          <h2 className={styles.imgDesc}>Handcrafting award winning digital experiences</h2>
+        </div>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Who Are We?</h1>
+          <p  className={styles.desc}>Description</p>
+        </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>What We Do?</h1>
+          <p  className={styles.desc}>
+          Description Description Description Description Description Description Description Description Description Description Description Description
+          <br/>
+          <br/> - Dynamic Websites
+          <br/>
+          <br/> - Fast and Handy
+          <br/>
+          <br/> - Mobile Apps
+          </p>
+
+          <Button url="/contact" text="Contact"/>
+        </div>
+      </div>
+     </div>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import styles from './page.module.css'
+import styles from './footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
         <div>2023 bug41. All rights reserved.</div>
-        <div></div>
+        <div className={styles.social}>          
+            <Image src="/1.png" alt="bug41 dev" width={15} height={15} className={styles.icon}/>
+            <Image src="/2.png" alt="bug41 dev" width={15} height={15} className={styles.icon}/>
+            <Image src="/3.png" alt="bug41 dev" width={15} height={15} className={styles.icon}/>
+            <Image src="/4.png" alt="bug41 dev" width={15} height={15} className={styles.icon}/>          
+        </div>
     </div>
   )
 }
