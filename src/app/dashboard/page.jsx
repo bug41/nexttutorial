dc109ab5190a12b6bb41f2bf92d000c75d76d46d
bug41 <bugkinguswit@gmail.com>
2023-08-01 23:12:39 +0900

    useEffect 가 아닌 swr 사용

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,14 +1,15 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import styles from "./page.module.css";
+import useSWR from 'swr';
 
 const Dashboard = () => {
 
-  const [data,setData] = useState([]);
-  const [err,setErr] = useState(false);
-  const [isLoading,setIsLoading] = useState(false);
+  //const [data,setData] = useState([]);
+  //const [err,setErr] = useState(false);
+  //const [isLoading,setIsLoading] = useState(false);
 
-  
+  /*
   useEffect(()=> {
     const getData = async () => {
       setIsLoading(true)
@@ -28,7 +29,11 @@ const Dashboard = () => {
 
     getData()
   }, []);
+  */
 
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+  const {data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts',fetcher)
   console.log(data);
 
   return (
